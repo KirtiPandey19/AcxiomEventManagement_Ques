836652424135df8a67fca276e40166dbc28ff5ad
Kirti Pandey <114803597+KirtiPandey19@users.noreply.github.com>
2023-12-16 20:34:25 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,355 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Complete responsive Event  Management Website create by Kirti Pandey</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+    <!-- header section starts  -->
+
+    <header>
+
+        <a href="#" class="logo">The Event Planner</a>
+
+        <div id="menu-bar" class="fas fa-bars"></div>
+
+        <nav class="navbar">
+            <a href="#home">home</a>
+            <a href="#vendor">vendor</a>
+            <a href="#clients">clients</a>
+            <a href="#AboutUs">About Us</a>
+            <a href="#Events">Events</a>
+            <a href="#contact">contact</a>
+
+        </nav>
+
+    </header>
+
+    <!-- header section ends -->
+
+    <!-- home section starts  -->
+
+    <section class="home" id="home">
+
+        <div class="content">
+           
+            <span>Event Management System</span>
+            <h3>we make every moment important and special !</h3>
+            <a href="#" class="btn">learm more</a>
+        </div>
+
+    </section>
+
+    <!-- home section ends -->
+
+    <section class="step-container">
+
+        <div class="step">
+            <span>01</span>
+            <h3>Events</h3>
+            <p>Talk Abour various Events and Events planner work under this organization</p>
+            <a href="#" class="btn">learm more</a>
+        </div>
+
+        <div class="step">
+            <span>02</span>
+            <h3>Save the date</h3>
+            <p>Save your date for pre advance booking</p>
+            <a href="#" class="btn">learm more</a>
+        </div>
+
+        <div class="step">
+            <span>03</span>
+            <h3>Our facilities</h3>
+            <p>This section includes various facilities provided by this organization</p>
+            <a href="#" class="btn">learm more</a>
+        </div>
+
+    </section>
+
+    <!-- vendor section starts  -->
+
+    <section class="vendor" id="vendor">
+
+        <div class="heading">
+            <h1>top Events vendors</h1>
+            <img src="header-img.png" alt="">
+        </div>
+
+        <div class="box-container">
+
+            <div class="box">
+                <img src="decoration.jpeg" alt="">
+                <div class="info">
+                    <h3>Decoration</h3>
+                    <p>Decoration is the first basic need for event.This website aims to provide that.</p>
+                    <a href="#" class="btn">learm more</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="florist.jpeg" alt="">
+                <div class="info">
+                    <h3>Florist</h3>
+                    <p>Floral related facilities also include</p>
+                    <a href="#" class="btn">learm more</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="catering.jpeg" alt="">
+                <div class="info">
+                    <h3>Catering</h3>
+                    <p>Proper Management of catering service</p>
+                    <a href="#" class="btn">learm more</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="venuehall.jpeg" alt="">
+                <div class="info">
+                    <h3>Venue and hall</h3>
+                    <p>Give Suggestion for various venues and halls</p>
+                    <a href="#" class="btn">learm more</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="Musicparty.jpeg" alt="">
+                <div class="info">
+                    <h3>Music and Party</h3>
+                    <p>Under this Event Planner also organizing music and parties </p>
+                    <a href="#" class="btn">learm more</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="photographer.jpeg" alt="">
+                <div class="info">
+                    <h3>Photographer</h3>
+                    <p>Providing Photographing facilities</p>
+                    <a href="#" class="btn">learm more</a>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- vendor section ends -->
+
+    <!-- client section starts  -->
+
+    <section class="clients" id="clients">
+
+        <div class="heading">
+            <h1>Our Previous Events</h1>
+            <img src="header-img.png" alt="">
+        </div>
+
+        <div class="row">
+
+            <div class="box-container">
+                <div class="box">
+                    <img src="corporateevnt.jpeg" alt="">
+                    <div class="info">
+                        <h3>01.Corporate Events</h3>
+                        <p> <i class="fas fa-quote-left"></i> Showcase successful corporate events such as product launches, team-building activities, and corporate parties.! <i class="fas fa-quote-right"></i> </p>
+                    </div>
+                </div>
+                <div class="box">
+                    <img src="Culturalevnts.jpeg" alt="">
+                    <div class="info">
+                        <h3>02. Cultural Events</h3>
+                        <p> <i class="fas fa-quote-left"></i> Display your capability in managing cultural events, art exhibitions, and performances. <i class="fas fa-quote-right"></i> </p>
+                    </div>
+                </div>
+            </div>
+
+            <img src="divider.png" class="divider" alt="">
+
+            <div class="box-container">
+                <div class="box">
+                    <div class="info">
+                        <h3>03. Sports Events</h3>
+                        <p> <i class="fas fa-quote-left"></i> If applicable, showcase your involvement in organizing sports events, tournaments, or competitions. <i class="fas fa-quote-right"></i> </p>
+                    </div>
+                    <img src="Sportsevnts.jpeg" alt="">
+                </div>
+                <div class="box">
+                    <div class="info">
+                        <h3>04. Community Events</h3>
+                        <p> <i class="fas fa-quote-left"></i>Feature events that contribute to the community, such as fundraisers, charity events, or festivals. <i class="fas fa-quote-right"></i> </p>
+                    </div>
+                    <img src="communityevnts.jpeg" alt="">
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- client section ends -->
+
+    <!-- AboutUS section starts  -->
+
+    <section class="AboutUs" id="AboutUs">
+
+        <div class="heading">
+            <h1>our creative team</h1>
+            <img src="header-img.png" alt="">
+        </div>
+
+        <div class="box-container">
+
+            <div class="box">
+                <img src="team.jpg" alt="">
+                <h3>Kirticoder</h3>
+                <span>Event planner</span>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta nisi ea quisquam ipsa molestias itaque, quod quis id doloremque similique!</p>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="team.jpg" alt="">
+                <h3>Kirticoder coder</h3>
+                <span>Event planner</span>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta nisi ea quisquam ipsa molestias itaque, quod quis id doloremque similique!</p>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="team.jpg" alt="">
+                <h3>Kirticoder</h3> coder</h3>
+                <span>Event planner</span>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Soluta nisi ea quisquam ipsa molestias itaque, quod quis id doloremque similique!</p>
+                <div class="share">
+                    <a href="#" class="fab fa-facebook-f"></a>
+                    <a href="#" class="fab fa-twitter"></a>
+                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="#" class="fab fa-linkedin"></a>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- AboutUS section ends -->
+
+    <!-- Events section starts  -->
+
+    <section class="Events" id="Events">
+
+        <div class="heading">
+            <h1>our latest events</h1>
+            <img src="header-img.png" alt="">
+        </div>
+
+        <div class="box-container">
+
+            <div class="box">
+                <img src="educevnts.jpeg" alt="">
+                <div class="info">
+                    <h3> <span>01.</span> Educational Events </h3>
+                    <p>Showcase seminars, workshops, or events with an educational focus.</p>
+                    <a href="#" class="btn">learn more</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="concert.jpeg" alt="">
+                <div class="info">
+                    <h3> <span>02. </span> Concerts and Entertainment Shows </h3>
+                    <p>Highlight your experience in managing concerts, live performances, or entertainment shows</p>
+                    <a href="#" class="btn">learn more</a>
+                </div>
+            </div>
+            <div class="box">
+                <img src="socialevnts.jpeg" alt="">
+                <div class="info">
+                    <h3> <span>03. </span> Social Events </h3>
+                    <p>Include weddings, birthdays, anniversaries, and other social gatherings that you have managed.</p>
+                    <a href="#" class="btn">learn more</a>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+    <!-- Event section ends-->
+
+    <!-- contact section starts  -->
+
+    <section class="contact" id="contact">
+
+        <div class="heading">
+            <h1>contact us</h1>
+            <img src="header-img.png" alt="">
+        </div>
+
+        <form action="contact_form.php" method="post" class="contact-form">
+            <div class="inputBox">
+                <input type="text" placeholder="your name" name="name">
+                <input type="email" placeholder="your email" name="email">
+            </div>
+            <div class="inputBox">
+                <input type="number" placeholder="your number" name="phone">
+                <input type="text" placeholder="your address" name="address">
+            </div>
+            <textarea placeholder="say something" name="message" id="" cols="30" rows="10"></textarea>
+            <input type="submit" value="send message" class="btn" name="send">
+        </form>
+
+    </section>
+
+    <!-- contact section ends -->
+
+
+
+    <!-- footer section  -->
+
+    <div class="footer"> created by <span> Kirti Pandey </span> | all rights reserved! </div>
+
+
+
+    <script>
+        let menu = document.querySelector('#menu-bar');
+        let navbar = document.querySelector('.navbar');
+
+        menu.addEventListener('click', () => {
+            menu.classList.toggle('fa-times');
+            navbar.classList.toggle('nav-toggle');
+        });
+
+        window.onscroll = () => {
+            menu.classList.remove('fa-times');
+            navbar.classList.remove('nav-toggle');
+        }
+    </script>
+
+
+</body>
+
+</html>
